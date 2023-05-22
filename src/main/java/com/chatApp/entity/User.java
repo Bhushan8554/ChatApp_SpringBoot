4189c52a5f629a32e9817fe151dc38549ee4dee5
bhushan8554 <bhushantaru4297@gmail.com>
2023-05-22 20:30:46 +0530

    added lombok annotations for getter setter and constructor in user class

--- src/main/java/com/chatApp/entity/User.java
+++ src/main/java/com/chatApp/entity/User.java
@@ -2,8 +2,14 @@ package com.chatApp.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
 	@Id
