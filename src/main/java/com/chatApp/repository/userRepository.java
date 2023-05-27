2ffa693a9288958f033656029d0401593e52843a
bhushan8554 <bhushantaru4297@gmail.com>
2023-05-27 13:41:39 +0530

    added repository for user

---
+++ src/main/java/com/chatApp/repository/userRepository.java
@@ -0,0 +1,10 @@
+package com.chatApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chatApp.entity.User;
+
+public interface userRepository extends JpaRepository<User, Integer>{
+
+	
+}
